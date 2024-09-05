c33bc959b37d0554c85aaf59cd423c7c2c0776ff
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-05 07:47:43 +0300

    checking for session id

--- src/main/java/com/willis/SpringSecurityExample/HelloController.java
+++ src/main/java/com/willis/SpringSecurityExample/HelloController.java
@@ -1,12 +1,13 @@
 package com.willis.SpringSecurityExample;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String greet(){
-        return "Hello world welcome";
+    public String greet(HttpServletRequest request){
+        return "Hello world welcome" + request.getSession().getId();
     }
 }
