26e167f5c69117547649a1b8759bb548757f00d7
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-05 07:03:21 +0300

    Spring security example

---
+++ src/test/java/com/willis/SpringSecurityExample/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willis.SpringSecurityExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
