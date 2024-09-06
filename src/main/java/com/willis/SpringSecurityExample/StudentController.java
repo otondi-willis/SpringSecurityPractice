0df1f2e672d0aa2319c3706957e2f1b5d3b8566e
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-06 09:02:00 +0300

    PostMapping controller

--- src/main/java/com/willis/SpringSecurityExample/StudentController.java
+++ src/main/java/com/willis/SpringSecurityExample/StudentController.java
@@ -1,8 +1,11 @@
 package com.willis.SpringSecurityExample;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.rmi.StubNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +18,11 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getStudents(){
         return students;
+    }
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
     }
 
 }
