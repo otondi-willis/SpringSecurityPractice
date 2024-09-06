724d5a843c205bf01ea51ca730e2296f900ee9b5
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-06 08:53:19 +0300

    GetMapping controller

--- src/main/java/com/willis/SpringSecurityExample/StudentController.java
+++ src/main/java/com/willis/SpringSecurityExample/StudentController.java
@@ -1,5 +1,20 @@
 package com.willis.SpringSecurityExample;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class StudentController {
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1,"otondi",70),
+            new Student(2,"willis",50)
+    ));
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
 
 }
