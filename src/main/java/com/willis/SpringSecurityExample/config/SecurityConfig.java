ae0d93fc97ca67b4131c038cbd328d5c5133ae44
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-06 09:44:51 +0300

    @EnableWebSecurity annotation to for custom configuration

--- src/main/java/com/willis/SpringSecurityExample/config/SecurityConfig.java
+++ src/main/java/com/willis/SpringSecurityExample/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.willis.SpringSecurityExample.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 }
