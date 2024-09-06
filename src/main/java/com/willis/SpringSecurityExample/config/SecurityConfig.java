e729821f83bbacad9e57bab701e7ccff2e584717
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-06 16:30:19 +0300

    making session id stateless

--- src/main/java/com/willis/SpringSecurityExample/config/SecurityConfig.java
+++ src/main/java/com/willis/SpringSecurityExample/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +18,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(request->request.anyRequest().authenticated());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
 
     }
