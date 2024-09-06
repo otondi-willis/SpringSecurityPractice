644348a7693c9d0b1b09576f4b6241749fc7df5b
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-06 09:58:28 +0300

    pick default properties and implement form login

--- src/main/java/com/willis/SpringSecurityExample/config/SecurityConfig.java
+++ src/main/java/com/willis/SpringSecurityExample/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.willis.SpringSecurityExample.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +15,9 @@ public class SecurityConfig {
 
         http.csrf(customizer->customizer.disable());
         http.authorizeHttpRequests(request->request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
         return http.build();
+
     }
 
 
