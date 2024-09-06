8cb49ae57d1a01d5a8972d15b9ce7ae8a91e1dee
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-06 09:48:27 +0300

    return a bean for security filter chain

--- src/main/java/com/willis/SpringSecurityExample/config/SecurityConfig.java
+++ src/main/java/com/willis/SpringSecurityExample/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.willis.SpringSecurityExample.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+
+
 
 }
