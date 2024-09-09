c9ee30938824866569a6eaa8ddd1473dc0e3f55f
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-09 04:55:39 +0300

    model package set up

--- src/main/java/com/willis/SpringSecurityExample/service/MyUserDetailsService.java
+++ src/main/java/com/willis/SpringSecurityExample/service/MyUserDetailsService.java
@@ -1,8 +1,14 @@
 package com.willis.SpringSecurityExample.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
