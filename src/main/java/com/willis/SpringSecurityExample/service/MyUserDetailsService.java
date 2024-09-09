2eb0f82b2625047e3d3a2d75798a146b3786d525
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-09 05:21:48 +0300

    find by user name method

--- src/main/java/com/willis/SpringSecurityExample/service/MyUserDetailsService.java
+++ src/main/java/com/willis/SpringSecurityExample/service/MyUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.willis.SpringSecurityExample.service;
 
+import com.willis.SpringSecurityExample.model.Users;
+import com.willis.SpringSecurityExample.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +10,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepo repo;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = repo.findByUsername(username);
         return null;
     }
 }
