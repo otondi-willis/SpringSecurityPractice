5847db71273335a9a0ee037bee1e26cd472c4dcd
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-09 05:14:09 +0300

    attributes plus getters and setters

---
+++ src/main/java/com/willis/SpringSecurityExample/model/Users.java
@@ -0,0 +1,35 @@
+package com.willis.SpringSecurityExample.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Users {
+    @Id
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
