9b75307271c6280098375aced3ad04992b33f60d
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-09 05:14:41 +0300

    toString generated

--- src/main/java/com/willis/SpringSecurityExample/model/Users.java
+++ src/main/java/com/willis/SpringSecurityExample/model/Users.java
@@ -32,4 +32,13 @@ public class Users {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
