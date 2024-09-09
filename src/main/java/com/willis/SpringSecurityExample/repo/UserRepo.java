c08b6354a25e60d4f26826c2b23be6e20ba208e1
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-09 05:21:40 +0300

    find by user name method

--- src/main/java/com/willis/SpringSecurityExample/repo/UserRepo.java
+++ src/main/java/com/willis/SpringSecurityExample/repo/UserRepo.java
@@ -1,6 +1,10 @@
 package com.willis.SpringSecurityExample.repo;
 
+import com.willis.SpringSecurityExample.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<Users,Integer> {
+    Users findByUsername(String username);
 }
