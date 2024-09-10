cef9ea091c2454b6d88d0c8f889573eccc47e726
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-10 03:37:13 +0300

    UsersService reference created

--- src/main/java/com/willis/SpringSecurityExample/controller/UserController.java
+++ src/main/java/com/willis/SpringSecurityExample/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.willis.SpringSecurityExample.controller;
 
 import com.willis.SpringSecurityExample.model.Users;
+import com.willis.SpringSecurityExample.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UsersService service;
     @PostMapping("/register")
     public Users register(@RequestBody Users user){
-        return user;
+        return service.register(user);
     }
 }
