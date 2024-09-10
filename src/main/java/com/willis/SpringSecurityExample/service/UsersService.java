20277b7f0d0b3673e6d00830709cd54d35a7f0c9
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-10 03:35:42 +0300

    UserRepo reference created

--- src/main/java/com/willis/SpringSecurityExample/service/UsersService.java
+++ src/main/java/com/willis/SpringSecurityExample/service/UsersService.java
@@ -1,12 +1,16 @@
 package com.willis.SpringSecurityExample.service;
 
 import com.willis.SpringSecurityExample.model.Users;
+import com.willis.SpringSecurityExample.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class UsersService {
+    @Autowired
+    private UserRepo repo;
     public Users register(Users user){
-
+        return repo.save(user);
     }
 }
