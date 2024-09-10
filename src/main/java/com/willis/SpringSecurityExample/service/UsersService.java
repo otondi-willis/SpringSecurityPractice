2c1152683c848aaabbde0a4d37c31590b09247dc
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-10 03:44:46 +0300

    use of BCrypt

--- src/main/java/com/willis/SpringSecurityExample/service/UsersService.java
+++ src/main/java/com/willis/SpringSecurityExample/service/UsersService.java
@@ -3,6 +3,7 @@ package com.willis.SpringSecurityExample.service;
 import com.willis.SpringSecurityExample.model.Users;
 import com.willis.SpringSecurityExample.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,10 @@ import org.springframework.stereotype.Service;
 public class UsersService {
     @Autowired
     private UserRepo repo;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     public Users register(Users user){
+        user.setPassword();
         return repo.save(user);
     }
 }
