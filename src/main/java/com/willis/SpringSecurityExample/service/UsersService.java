77a66ec3d4e9bc9d1f4691d5d9d285377221ab36
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-10 03:32:36 +0300

    UsersService class annotated

--- src/main/java/com/willis/SpringSecurityExample/service/UsersService.java
+++ src/main/java/com/willis/SpringSecurityExample/service/UsersService.java
@@ -1,4 +1,12 @@
 package com.willis.SpringSecurityExample.service;
 
+import com.willis.SpringSecurityExample.model.Users;
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class UsersService {
+    public Users register(Users user){
+
+    }
 }
