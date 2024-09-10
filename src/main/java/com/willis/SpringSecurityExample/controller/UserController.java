fcd5f1aee22df55868d35e0c920e0253714d1cb9
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-10 03:28:47 +0300

    UserController class created as rest controller

---
+++ src/main/java/com/willis/SpringSecurityExample/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.willis.SpringSecurityExample.controller;
+
+import com.willis.SpringSecurityExample.model.Users;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user){
+
+    }
+}
