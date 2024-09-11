7e5a6ee66f35f3494393c75f47af2ed595d33657
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-11 03:24:06 +0300

    jwt token generation

--- src/main/java/com/willis/SpringSecurityExample/service/JWTService.java
+++ src/main/java/com/willis/SpringSecurityExample/service/JWTService.java
@@ -1,11 +1,19 @@
 package com.willis.SpringSecurityExample.service;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JWTService {
 
-    public static String generateToken() {
-
+    public static String generateToken(String username) {
+        Map<String, Object> claims = new HashMap();
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject()
     }
 }
