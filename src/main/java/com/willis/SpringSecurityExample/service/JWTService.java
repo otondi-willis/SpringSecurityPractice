8437f427b96fefab74e4bf81dadda3140ff7f3a2
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-11 04:09:24 +0300

    edited the date generation part

--- src/main/java/com/willis/SpringSecurityExample/service/JWTService.java
+++ src/main/java/com/willis/SpringSecurityExample/service/JWTService.java
@@ -36,7 +36,7 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()*60*60*30))
+                .expiration(new Date(System.currentTimeMillis()+60*60*30))
                 .and()
                 .signWith(getKey())
                 .compact();
